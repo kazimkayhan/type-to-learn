19a24c50877eb64bdb281497d95dce60f0f00d99
Kazim Kayhan <email4kazim@gmail.com>
2026-09-26 10:52:39 +0430

    make 4000 EEW featured

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -15,7 +15,7 @@ export const EXPLICIT_SPACE = "␣";
 
 export const CHAPTER_LENGTH = 20;
 
-export const DEFAULT_DICT_ID = "pet-2024";
+export const DEFAULT_DICT_ID = "eew4000-meaning";
 
 export const DISMISS_START_CARD_DATE_KEY = "dismissStartCardDate";
 
